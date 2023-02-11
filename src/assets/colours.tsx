4d24744287b536c784a542ad7f451134badbb256
Faizan Rasool <faizanrasool@Faizans-MBP.cogeco.local>
2023-02-10 23:00:37 -0500

    finished the about section

--- src/assets/colours.tsx
+++ src/assets/colours.tsx
@@ -1,3 +1,8 @@
 export const colours = {
-  secondaryText: "#8b8b8b",
+  primaryText: "#cecece",
+  secondaryText: "#b6b6b6",
+  subHeaderText: "#9f9f9f",
+  buttonText: "#8b8b8b",
+  buttonHover: "#8b8b8b",
+  underlineColour: "#4d4d4d",
 };
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,13 +1,56 @@
 import React from "react";
 
 import { Nav } from "../components/Nav";
+import mcmasterLogo from "../assets/images/mcmaster-icon.png";
 
 function About() {
   return (
-    <div className="">
+    <div className="font-opensans">
       <Nav />
-      <div className="sm:w-full md:w-6/12 m-auto py-6">
-        <h1 className="text-2xl font-semibold">Faizan Rasool</h1>
+      <div className="sm:w-8/12 md:w-6/12 m-auto pt-8 pb-6">
+        <h1 className="text-3xl font-bold text-[#e4e4e4]">Faizan Rasool</h1>
+      </div>
+      <div className="sm:w-8/12 md:w-6/12 m-auto text-base leading-7 ">
+        <p className="text-[#cecece]">
+          Hey, I'm Faizan and I'm an electrical engineering student at{" "}
+          <a
+            className="underline decoration-solid decoration-1.5 decoration-[#4d4d4d] underline-offset-4 transition ease-in-out delay-50 hover:decoration-[#e4e4e4] duration-300"
+            href="https://www.eng.mcmaster.ca/ece"
+          >
+            McMaster University.{" "}
+          </a>
+          Inspired by a passion for solving complex problems and driven by a
+          love for technology, I aim to make a difference as an engineer.
+        </p>
+        <br />
+        <p>
+          Currently, I'm wrapping up my final semester of school and searching
+          for full time opportunities. When I'm not busy, I like to hit the gym
+          with some friends or watch some basketball.
+        </p>
+        <br />
+        <p>
+          If you would like to get in touch, you can reach me at{" "}
+          <a
+            className="underline decoration-solid decoration-1.5 decoration-[#4d4d4d] underline-offset-4 transition ease-in-out delay-50 hover:decoration-[#e4e4e4] duration-300"
+            href="mailto:faizanraso@outlook.com"
+          >
+            faizanraso@outlook.com
+          </a>
+        </p>
+      </div>
+      <div className="education-section sm:w-8/12 md:w-6/12 mt-6 mx-auto pt-8 pb-6">
+        <h2 className="text-2xl font-bold text-[#9f9f9f]">Education</h2>
+        <div className="flex flex-row">
+          <div>
+            <img src={mcmasterLogo} width={60} />
+          </div>
+          <div className="flex flex-column">
+            <div className="flex flex-row">
+              <h4>McMaster University</h4>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
